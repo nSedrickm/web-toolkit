9dfdf950dfc28eb3d6f98b3b5e36e2a1d64027f3
nsedrickm <nsedrick101@gmail.com>
2020-06-04 13:08:18 +0100

    change login mech

--- components/login/routes/routes.js
+++ components/login/routes/routes.js
@@ -1,13 +1,42 @@
 var express = require("express");
 var router = express.Router();
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Login" });
 });
 
-router.post("/login", function(req, res, next) {
-  res.send("respond with a resource");
+router.post("/login", urlencodedParser, function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+  var passwordError = "Wrong password";
+  var emailError = "Wrong email";
+
+  //loggin contents received from form
+  console.log(req.body);
+
+  //some basic validateion
+
+  if (
+    (password != "######" || password == "") &&
+    (email != "email@example.com" || email == "")
+  ) {
+    console.log(emailError + " and " + passwordError + " entered.");
+  } else if (email != "email@example.com" || email == "") {
+    console.log(emailError);
+  } else if (password != "######" || password == "") {
+    console.log(passwordError);
+  } else {
+    console.log("login Successful");
+  }
+
+  res.render("index", {
+    title: "Login",
+    emailError: emailError,
+    passwordError: passwordError
+  });
 });
 
 module.exports = router;
